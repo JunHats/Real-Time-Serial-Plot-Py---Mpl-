a42aa051c659fe787bf2e08114dad3e6c98ee084
raul <raulliston@hotmail.com>
2020-10-02 08:47:09 -0300

    Trying do correct error on serial reading

--- serialCSV.py
+++ serialCSV.py
@@ -2,7 +2,7 @@ import serial
 import time
 import csv
 
-ser = serial.Serial(port="COM3", baudrate=115200)
+ser = serial.Serial(port="COM3", baudrate=57600)
 ser.flushInput()
 fieldnames = ["x_value", "pressure_1", "pressure_2", "pressure_3"]
 
@@ -12,14 +12,17 @@ with open('data.csv', 'w') as csv_file:
 
 
 while True:
+
     ser_bytes = ser.readline()
+    print(ser_bytes)
     reading = (ser_bytes.decode('utf-8').strip('\r\n').split(','))
     with open('data.csv', 'a') as csv_file:
         csv_writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
         x_value = reading[0]
-        pressure_1 = reading[1]
-        pressure_2 = reading[2]
-        pressure_3 = reading[3]
+        if(len(reading) == 4):
+            pressure_1 = reading[1]
+            pressure_2 = reading[2]
+            pressure_3 = reading[3]
 
         info = {
             "x_value": x_value,
@@ -27,10 +30,10 @@ while True:
             "pressure_2": pressure_2,
             "pressure_3": pressure_3
             }
-
+        #time.sleep(0.001)
         csv_writer.writerow(info)
         print(x_value, pressure_1, pressure_2, pressure_3)
-        time.sleep(0.01)
+
 
 
 
